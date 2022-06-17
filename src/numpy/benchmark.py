4f233b505e1e1a051415ce1e5744dcf278b83574
Kangqi Fu <kfu9@wisc.edu>
2022-06-17 13:25:32 +0800

    update numba and taichi file code

--- src/numpy/benchmark.py
+++ src/numpy/benchmark.py
@@ -1,6 +1,7 @@
 from .poisson_disk import run_poisson
 
 {'numpy_cpu': [{'desired_samples': 1000, 'time_ms': 991.2736028200015}, {'desired_samples': 5000, 'time_ms': 5998.027631384321}, {'desired_samples': 10000, 'time_ms': 12623.283750191331}, {'desired_samples': 50000, 'time_ms': 70075.99696719553}, {'desired_samples': 100000, 'time_ms': 74836.31602639798}]}
+#TODO: above need to be updated to new value
 
 def benchmark():
     n_samples = [1000, 5000, 10000, 50000, 100000]
